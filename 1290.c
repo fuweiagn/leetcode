18d674a944e330856a3b5dbc6314561e1bdb24cb
fuwei <fuxiaowei1996@gmail.com>
2020-03-21 23:56:23 +0800

    improved the readability

--- 1290.c
+++ 1290.c
@@ -11,20 +11,12 @@ int main(int argc, char *argv[])
     return 0;
 }
 int getDecimalValue(struct ListNode* head){
-    int list[30] = {0};
-    int i = 0;
-    while (head != NULL)
+    int result = head->val;
+    struct ListNode *n = head->next;
+    while (n != NULL)
     {
-        list[i] = head->val;
-        head = head->next;
-        ++i;
+        result = result * 2 + n->val;
+        n = n->next;
     }
-    i -= 2;
-    int result = 0;
-    for(int j = i, k = 0; j > -1; --j, ++k)
-    {
-        result += (list[j] * (2 << k));
-    }
-    result += (list[i+1] * 1);
     return result;
 }
