54532cd7884f28a4d060e036b0093adda44e322c
fuwei <fuxiaowei1996@gmail.com>
2020-03-21 17:23:19 +0800

    do not change the parameter name :)

--- 1365.c
+++ 1365.c
@@ -1,43 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-int *solve(int *num, int numsSize, int *returnSize);
+int solve(char *j, char *s);
 int main(int argc, char *argv[])
 {
-    int nums[4];
-    nums[0] = 6;
-    nums[1] = 5;
-    nums[2] = 4;
-    nums[3] = 8;
-    int returnSize;
-    int *result = solve(nums, 4, &returnSize);
-    for (size_t i = 0; i < 4; i++)
-    {
-        printf("%d", result[i]);
-    }
-    
     return 0;
 }
-int *solve(int *nums, int numsSize, int *returnSize)
+int solve(char *J, char *S)
 {
-    int *result = malloc(numsSize * sizeof(int));
-    int count[101] = {0};
-    for (size_t i = 0; i < numsSize; i++)
+    char isj[128] = {0};
+    char c = *J;
+    while (c != '\0')
     {
-        ++count[nums[i]];
+        isj[c] = 1;
+        c = *(++J);
     }
-    for (size_t i = 1; i < 101; i++)
+    c = *S;
+    int count = 0;
+    while (c != '\0')
     {
-        count[i] += count[i-1];
+        if(isj[c] != 0) ++count;
+        c = *(++S);
     }
-    for (size_t i = 0; i < numsSize; i++)
-    {
-        if(nums[i] == 0)
-        {
-            result[i] = 0;
-        } else {
-            result[i] = count[nums[i]-1];
-        }
-    }
-    *returnSize = numsSize;
-    return result;
+    return count;
+     
 }
