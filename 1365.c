19f94719ea4f847d95fe7b80bb19cd2900fd4130
fuwei <fuxiaowei1996@gmail.com>
2020-03-21 17:43:22 +0800

    it seems simple that you can directly use the mod operator

--- 1365.c
+++ 1365.c
@@ -1,26 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-int solve(char *j, char *s);
+int solve(int n);
 int main(int argc, char *argv[])
 {
     return 0;
 }
-int solve(char *J, char *S)
+int solve(int n)
 {
-    char isj[128] = {0};
-    char c = *J;
-    while (c != '\0')
+    //use mod operator
+    int digit;
+    int sum = 0;
+    int prod = 1;
+    while (n != 0)
     {
-        isj[c] = 1;
-        c = *(++J);
+        digit = n % 10;
+        sum += digit;
+        prod *= digit;
+        n /= 10;
     }
-    c = *S;
-    int count = 0;
-    while (c != '\0')
-    {
-        if(isj[c] != 0) ++count;
-        c = *(++S);
-    }
-    return count;
-     
+    return prod - sum;
 }
