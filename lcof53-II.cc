429fb63d908d6e22462970afc54fab10ffb602ba
fwei <fuweiagn@outlook.com>
2020-08-04 22:14:41 +0800

    binary search

---
+++ lcof53-II.cc
@@ -0,0 +1,27 @@
+#include "leetcode.hpp"
+
+int main()
+{
+
+    return 0;
+}
+int missingNumber(vector<int> &nums)
+{
+    int i = 0;
+    int j = nums.size() - 1;
+
+    while (i <= j)
+    {
+        int m = (i + j) / 2;
+        if (nums[m] == m)
+        {
+            i = m + 1;
+        }
+        else
+        {
+            j = m - 1;
+        }
+    }
+
+    return i;
+}
