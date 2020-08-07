e893db856256b262001ebfd5fa764d652440ac5e
fwei <fuweiagn@outlook.com>
2020-08-07 13:02:10 +0800

    inplace

--- lcof58-II.cc
+++ lcof58-II.cc
@@ -4,8 +4,27 @@ int main()
 {
     return 0;
 }
-string reverseLeftWords(string s, int n) {
+void reverse(string &s, int begin, int end)
+{
+    while (begin < end)
+    {
+        swap(s[begin], s[end]);
+        begin++;
+        end--;
+    }
+}
+string reverseLeftWords(string s, int n)
+{
+    // time: O(N), space: O(N)
     string ret(s.begin() + n, s.end());
     ret += string(s.begin(), s.begin() + n);
     return ret;
+
+// inplace
+#ifdef INPLACE
+    reverse(s, 0, n - 1);
+    reverse(s, n, s.length() - 1);
+    reverse(s, 0, s.length() - 1);
+    return s;
+#endif
 }
